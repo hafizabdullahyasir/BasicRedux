fdbda8292a4ab7df8dace3260d4131fec4ebf9d5
Hafiz Abdullah Yasir <hafizabdullahyasir.7@gmail.com>
2025-06-05 13:36:35 +0500

    created button as a component

---
+++ components/Buttons.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+
+
+export default function Buttons(){
+
+
+
+
+return(
+   
+    <div>
+        <h1 className='h1'>Counter Application</h1>
+        <h1 className="h1">Count:</h1>
+        <div className="button">
+     <button >
+        increment 
+     </button>
+     <button >
+      decrement 
+     </button>
+     <button >
+        Reset 
+     </button>
+
+
+    </div>
+    </div>
+    
+)
+
+
+}
+
+
