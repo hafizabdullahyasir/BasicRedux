4e10ef2e7e86bc5dcb8b262db38abc746d3be30c
Hafiz Abdullah Yasir <hafizabdullahyasir.7@gmail.com>
2025-06-05 13:34:55 +0500

    removed strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import store from './redux/store.js'
 import { Provider } from 'react-redux'
@@ -6,9 +6,9 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
     <Provider store={store}>
     <App />
     </Provider>
-  </StrictMode>,
+  
 )
